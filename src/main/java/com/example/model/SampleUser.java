ce0d7b1dc9238f7f0ea2b406eb0684a65fb66f51
이해경 <ondal@dhcp-9-240-100-27.j1c-kr.ibmmobiledemo.com>
2023-12-12 10:57:35 +0900

    Mybatis sample with Spring boot 3.2.0

---
+++ src/main/java/com/example/model/SampleUser.java
@@ -0,0 +1,20 @@
+package com.example.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+public class SampleUser {
+	private String userId ;
+	private String userNm ;
+	private String addr ;
+	private String cellPhone ;
+	private String agreeInform;
+	private String birthDt ;
+}
